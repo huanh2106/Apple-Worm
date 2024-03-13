e6f5bf4d0a736b58e0ef2574ab7f792c4f499fb3
huanh2106 <23020080@vnu.edu.vn>
2024-03-13 23:23:28 +0700

    Update lan 1

---
+++ BaseObject.h
@@ -0,0 +1,19 @@
+#ifndef BASE_OBJECT_H_
+#define BASE_OBJECT_H_
+#include "CommonFunc.h"
+class BaseObject
+{
+public:
+		BaseObject();
+		~BaseObject();
+		void SetRect(const int& x, const int& y) { rect_.x = x, rect_.y = y; }
+		SDL_Rect GetRect() const { return rect_; }
+		SDL_Texture* GetObject() const { return p_object; }
+		bool LoadImg(std::string path, SDL_Renderer* screen);
+		void Render(SDL_Renderer* des, const SDL_Rect* clip = NULL);
+		void Free();
+protected:
+	SDL_Rect rect_;
+	SDL_Texture* p_object;
+};
+#endif // BASE_OBJECT_H_
--- main.cpp
+++ main.cpp
@@ -1,121 +1,78 @@
-/*This source code copyrighted by Lazy Foo' Productions 2004-2024
-and may not be redistributed without written permission.*/
+#include "BaseObject.h"
+#include "CommonFunc.h"
+BaseObject g_background;
 
-//Using SDL and standard IO
-#include <SDL2/SDL.h>
-#include <stdio.h>
-
-//Screen dimension constants
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
-
-//Starts up SDL and creates window
-bool init();
-
-//Loads media
-bool loadMedia();
-
-//Frees media and shuts down SDL
-void close();
-
-//The window we'll be rendering to
-SDL_Window* gWindow = NULL;
-	
-//The surface contained by the window
-SDL_Surface* gScreenSurface = NULL;
-
-//The image we will load and show on the screen
-SDL_Surface* gHelloWorld = NULL;
-
-bool init()
+bool initData()
 {
-	//Initialization flag
 	bool success = true;
-
-	//Initialize SDL
-	if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
+	int ret = SDL_Init(SDL_INIT_VIDEO);
+	if(ret<0)
+		return false;
+	SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "1");
+	g_window = SDL_CreateWindow("Apple Worm by Huy", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+	if (g_window == NULL)
 	{
-		printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
 		success = false;
 	}
 	else
 	{
-		//Create window
-		gWindow = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
-		if( gWindow == NULL )
+		g_screen = SDL_CreateRenderer(g_window, -1, SDL_RENDERER_ACCELERATED);
+		if (g_screen == NULL)
 		{
-			printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
 			success = false;
 		}
 		else
 		{
-			//Get window surface
-			gScreenSurface = SDL_GetWindowSurface( gWindow );
+			SDL_SetRenderDrawColor(g_screen, RENDER_DRAW_COLOR, RENDER_DRAW_COLOR, RENDER_DRAW_COLOR, RENDER_DRAW_COLOR);
+			int imgFlags = IMG_INIT_PNG;
+			if (!(IMG_Init(imgFlags) && imgFlags))
+			{
+				success = false;
+			}
 		}
 	}
-
-	return success;
-}
-
-bool loadMedia()
-{
-	//Loading success flag
-	bool success = true;
-
-	//Load splash image
-	gHelloWorld = SDL_LoadBMP( "hello_world.bmp" );
-	if( gHelloWorld == NULL )
 	{
-		printf( "Unable to load image %s! SDL Error: %s\n", "02_getting_an_image_on_the_screen/hello_world.bmp", SDL_GetError() );
-		success = false;
+
 	}
-
-	return success;
 }
-
+bool LoadBackground()
+{
+	bool ret = g_background.LoadImg("image//background.png", g_screen);
+	if (ret == false)
+		return false;
+	return true;
+}
 void close()
 {
-	//Deallocate surface
-	SDL_FreeSurface( gHelloWorld );
-	gHelloWorld = NULL;
-
-	//Destroy window
-	SDL_DestroyWindow( gWindow );
-	gWindow = NULL;
-
-	//Quit SDL subsystems
+	g_background.Free();
+	SDL_DestroyRenderer(g_screen);
+	g_screen = NULL;
+	SDL_DestroyWindow(g_window);
+	g_window = NULL;
+	IMG_Quit();
 	SDL_Quit();
 }
-
-int main( int argc, char* args[] )
+int main(int argc, char* argv[])
 {
-	//Start up SDL and create window
-	if( !init() )
+	if (initData() == false)
+		return -1;
+	if (LoadBackground() == false)
+		return -1;
+	bool is_quit = false;
+	while (!is_quit)
 	{
-		printf( "Failed to initialize!\n" );
-	}
-	else
-	{
-		//Load media
-		if( !loadMedia() )
+		while (SDL_PollEvent(&g_event) != 0)
 		{
-			printf( "Failed to load media!\n" );
+			if (g_event.type == SDL_QUIT)
+			{
+				is_quit = true;
+			}
 		}
-		else
-		{
-			//Apply the image
-			SDL_BlitSurface( gHelloWorld, NULL, gScreenSurface, NULL );
-			
-			//Update the surface
-			SDL_UpdateWindowSurface( gWindow );
-
-            //Hack to get window to stay up
-            SDL_Event e; bool quit = false; while( quit == false ){ while( SDL_PollEvent( &e ) ){ if( e.type == SDL_QUIT ) quit = true; } }
-		}
+		SDL_SetRenderDrawColor(g_screen, RENDER_DRAW_COLOR, RENDER_DRAW_COLOR, RENDER_DRAW_COLOR, RENDER_DRAW_COLOR);
+		SDL_RenderClear(g_screen);
+		g_background.Render(g_screen, NULL);
+		SDL_RenderPresent(g_screen);
 	}
-
-	//Free resources and close SDL
 	close();
-
 	return 0;
 }
